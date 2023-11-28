f366af698750c5a1e76e2c71a22a72748ef785c3
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-11-28 19:39:51 +0530

    created simple list using node class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("LinkedList Program.");
+        // UC1: creating linkedlist with nodes 56, 30 and 70
+        Node<Integer> node1 = new Node<>(56);
+        Node<Integer> node2 = new Node<>(30);
+        Node<Integer> node3 = new Node<>(70);
+        node1.next = node2;
+        node2.next = node3;
+
+        Node<Integer> node = node1;
+        while (node != null) {
+            System.out.print(node.data + " -> ");
+            node = node.next;
+        }
     }
 }
