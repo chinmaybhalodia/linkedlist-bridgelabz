7f9cc0878073e736a102a70053b55a520f215272
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-11-29 01:38:00 +0530

    created ordered list class using linked list

--- src/App.java
+++ src/App.java
@@ -1,18 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // UC2: creating linkedlist and adding nodes
-        LinkedList<Integer> list = new LinkedList<>();
+        // UC10: implementing ordered list using linkedlist
+        OrderedList<Integer> list = new OrderedList<>();
         list.add(56);
+        System.out.println(list);
         list.add(30);
+        System.out.println(list);
+        list.add(40);
+        System.out.println(list);
         list.add(70);
-        System.out.println(list);
-
-        // UC8: insert node 40 after node 30
-        list.insertAfterNode(30, 40);
-        System.out.println(list);
-
-        // UC9: delete node 40
-        list.deleteNode(40);
         System.out.println(list);
     }
 }
