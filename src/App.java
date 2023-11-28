502c4570284ead3e50952962880deef9336da83a
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-11-28 20:04:37 +0530

    created function to add node at start of list

--- src/App.java
+++ src/App.java
@@ -1,16 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // UC1: creating linkedlist with nodes 56, 30 and 70
-        Node<Integer> node1 = new Node<>(56);
-        Node<Integer> node2 = new Node<>(30);
-        Node<Integer> node3 = new Node<>(70);
-        node1.next = node2;
-        node2.next = node3;
-
-        Node<Integer> node = node1;
-        while (node != null) {
-            System.out.print(node.data + " -> ");
-            node = node.next;
-        }
+        // UC2: creating linkedlist and adding nodes
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addFirst(70);
+        list.addFirst(30);
+        list.addFirst(56);
+        System.out.println(list);
     }
 }
---
+++ src/LinkedList.java
@@ -0,0 +1,33 @@
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // method to add new node at head of linkedlist
+    public void addFirst(T data) {
+        Node<T> node = new Node<>(data);
+        // if linkedlist is empty
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String listdata = "";
+        Node<T> node = head;
+        while (node != null) {
+            listdata += node.data + " ";
+            node = node.next;
+        }
+        return listdata;
+    }
+}
